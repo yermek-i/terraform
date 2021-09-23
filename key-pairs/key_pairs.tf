8bad79609d1de5ca1808e7110f316fc200c7f8f3
yermek-i <56139134+yermek-i@users.noreply.github.com>
2021-09-23 18:38:07 -0500

    Update key_pairs.tf

--- key-pairs/key_pairs.tf
+++ key-pairs/key_pairs.tf
@@ -12,7 +12,7 @@ resource "aws_key_pair" "developer1" {
 
 resource "aws_key_pair" "developer2" {
   key_name   = "developer2"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxHxP26CfOCiFRvOdalX6yC6Z9cI+ShpnJJdZH9LRtzVhcHALHDbH0Kt9VlBcKIKlNLAv8y39ESBiS32iyrP7E6hcmWSj1ghaXT2mIWVLjRC/IxBQP7TvCrH+5JHRWA/UQ8lpGrzdzr+PJcR7M1YS8z0vjT72ToctpL+bOftzbIRKxUEfHswZoXOO5s3apNwLr93TG6f+D+sj2AK0O4XM+RW3oJeJNAV8HPa6/1M8omfxiuZxV8nAwNQUS5NZVuENZlXfkg6+wuxqK3pkiRfsUimquEWKU3GsLwmjBqXJ+OgWBC2HoNFqYeTQBEbITLv80eC8XrZFGYYgkbBKFczpt farrukh90@Farrukhs-MacBook-Pro.local"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCWN51unxqKnxJPellH29MOAjAXb8dsiqMGxtUQo7Re++MLxTw0I8TKA1q/OG3vT9U7axA2Id80ihu9bUt1R7EwxeULtM1Xxy0lPXhGdefhI2AcVXgo5ZseP+mmBmmNdGdrdEbZExHJ4V6jhZ+cT3ZxQxRfEkjSZ2/xw8Nw9MQrLuPKhXor23OWltfLVrYm7q+pBq+0dsJYISCKluJZAd3oFLwoA6qSXDOwfGIxalVdcGFZ9WWU9TobTeG6QIE6R91ym9TL9+wxGMg+O4LOPtDpg9qYwO0XC3fCxrA9sXMK1fLEWfVWMOQ1BAMMopE04ZUFa9F6gTqcXC+3wYeF9vpzmRvgU6YgWCpLxiPG7v2K5ZNwlW04reRIqRfVsfbtlV8wnhR/Q82LL0/EWsJnUrrKM+lb2klwxzuEYzpwO/9Kc6XM1JqXVyAbtnpHggg4T2k3ftFUwqU1h2HS8TGqf6iDiJ6ezLR/LOQa+K6V1Tq1/hJHag9aQP/5gJSq9gF80oE= yermek@Yermeks-MacBook-Pro.local"
   tags = {
       Deployed = "Terraform"
       Team = "DevOps"
